bfefd2a6885bb41819b9cf0b008a4a8cd58ac06a
iamfaisal091 <davidfk6603994@gmail.com>
2025-12-28 18:29:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,29 @@
-const CACHE_NAME = 'chatkaro-v1';
-const urlsToCache = [
-  '/',
-  '/chatkaro.pages.dev/',
-  '/manifest.json',
-  'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js',
-  'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js',
-  'https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+const cacheName = 'chathub-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  e.respondWith(networkFirst(req));
 });
 
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+async function networkFirst(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached || caches.match('./index.html');
+  }
+}
